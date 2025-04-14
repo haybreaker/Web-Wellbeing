fbd78c822b5d2c799f69e38cf40d49d8ba1b9ec7
Jesse Hayward <jessehayward@hookd.app>
2025-04-14 20:19:02 +1000

    content-script fix

--- common.js
+++
@@ -1,7 +0,0 @@
-function getTodayDateString() {
-    const today = new Date();
-    today.setMinutes(today.getMinutes() - today.getTimezoneOffset()); // Convert to local time
-    return today.toISOString().split('T')[0]; // Extract YYYY-MM-DD
-}
-
-export { getTodayDateString };
--- content-script.js
+++ content-script.js
@@ -1,9 +1,15 @@
 const activityEvents = ['mousedown', 'scroll', 'contextmenu'];
+const port = chrome.runtime.connect({"name": "content-script"});
+const portDead = false;
 
 const handleActivity = () => {
-  chrome.runtime.sendMessage({ type: 'userActivity' });
+  if(!portDead) chrome.runtime.sendMessage({ type: 'userActivity' });
 };
 
 activityEvents.forEach(event => {
   document.addEventListener(event, handleActivity, { passive: true });
 });
+
+port.onDisconnect.addListener(function() {
+    portDead = true;
+})
